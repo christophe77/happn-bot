93c78a6fe53fc5316b777ddbafd2ba5a6b6240fd
christophe77 <chrisb@gmx.fr>
2022-05-16 00:21:58 +0200

    criterias

---
+++ src/const/criterias.js
@@ -0,0 +1,54 @@
+const criteriaList = [
+    {
+        id: "73210140-772c-11e9-9403-ab4bdd6fe9b1", default_short_label: "Looking for", answers: [
+            { relationship: { id: "1c90dc00-772d-11e9-9403-ab4bdd6fe9b1", default_label: "A relationship" } },
+            { nothingSerious: { id: "58c4ffd0-772d-11e9-9403-ab4bdd6fe9b1", default_label: "Nothing serious" } },
+            { everything: { id: "5efb1e20-772d-11e9-9403-ab4bdd6fe9b1", default_label: "I’ll know when I find it" } },
+        ]
+    },
+    { id: "a23938b0-77f6-11e9-b0dc-35a91441a495", default_short_label: "Height" },
+    {
+        id: "bae69d90-77e6-11e9-b4c2-c3a9c0a73afd", default_short_label: "Exercise", answers: [
+            { occasional: { id: "27afc720-77e8-11e9-b4c2-c3a9c0a73afd", default_label: "Occasional exercise" } },
+            { enoughCardio: { id: "3f3fe1e0-77e8-11e9-b4c2-c3a9c0a73afd", default_label: "Enough cardio to keep up" } },
+            { allTime: { id: "45f1e5a0-7956-11e9-8eb2-d3c69ddd8234", default_label: "All exercise all the time" } },
+        ]
+    },
+    {
+        id: "d61867c0-77f4-11e9-b43e-ed07d0af7b4d", default_short_label: "Cooking", answers: [
+            { microwaveMaster: { id: "090f5350-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "I’m a microwave master" } },
+            { deliveryExpert: { id: "51b07710-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "I’m a delivery expert" } },
+            { goodSkills: { id: "5f045490-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "I know a few good recipes" } },
+            { chef: { id: "66069c80-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "I’m an excellent chef" } },
+        ]
+    },
+    {
+        id: "98aac8a0-77eb-11e9-b4c2-c3a9c0a73afd", default_short_label: "Travel", answers: [
+            { backpack: { id: "af7374b0-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "Hiking & backpack" } },
+            { sunscreen: { id: "b5e0a890-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "Deckchair & sunscreen" } },
+            { museums: { id: "bc9ff410-77eb-11e9-b4c2-c3a9c0a73afd", default_label: "Museum & postcards" } },
+        ]
+    },
+    {
+        id: "f5ce5f90-91e2-11e9-86f7-9119d852bbdd", default_short_label: "Partying", answers: [
+            { bedAtMidnight: { id: "35ba0d20-91e3-11e9-86f7-9119d852bbdd", default_label: "I’m in bed by midnight" } },
+            { moderate: { id: "41625d80-91e3-11e9-86f7-9119d852bbdd", default_label: "I party in moderation" } },
+            { nightOwl: { id: "47948430-91e3-11e9-86f7-9119d852bbdd", default_label: "I’m a night owl" } },
+        ]
+    },
+    {
+        id: "79c98710-7ef1-11ea-9cbc-072a91dcea22", default_short_label: "Smoking", answers: [
+            { ISmoke: { id: "b5952510-7ef1-11ea-9cbc-072a91dcea22", default_label: "Well, I smoke" } },
+            { notAFan: { id: "e03db520-7ef1-11ea-9cbc-072a91dcea22", default_label: "Not a fan, but whatever" } },
+            { zeroTolerance: { id: "f502f010-7ef1-11ea-9cbc-072a91dcea22", default_label: "Zero tolerance" } },
+        ]
+    },
+    {
+        id: "31454f40-7ef3-11ea-9cbc-072a91dcea22", default_short_label: "Kids", answers: [
+            { loveMines: { id: "1d1f6f50-7ef8-11ea-9cbc-072a91dcea22", default_label: "I love the ones I have" } },
+            { wantMore: { id: "c1ad2300-7ef8-11ea-9cbc-072a91dcea22", default_label: "I have some, but want more" } },
+            { wantSome: { id: "e49e4bf0-7ef8-11ea-9cbc-072a91dcea22", default_label: "I’d like some, thanks" } },
+            { noThanks: { id: "3edb7e30-7ef9-11ea-9cbc-072a91dcea22", default_label: "Thanks, but no thanks" } },
+        ]
+    }
+]
