026a28ee6a27a60dd238bf976c42ed81d59b5329
christophe bellec <christophe.bellec@ekim.fr>
2022-05-23 08:31:23 +0200

    processRecommandations

--- src/core/processRecommandations.js
+++ src/core/processRecommandations.js
@@ -1,25 +1,44 @@
 const { recommandations } = require('happn-api').default;
 const colors = require('../utils/colors');
-const handleRecommandation = require("./handleRecommandation");
+const handleRecommandation = require('./handleRecommandation');
 
 async function processRecommandations(savePictures, criterias) {
-    let renewableLikes = -1;
-    const myRecommandations = await recommandations();
-    await Promise.all(myRecommandations.data.map(async (recommandation) => {
+  let renewableLikes = -1;
+  const myRecommandations = await recommandations();
+  if (myRecommandations.data?.length === 0) {
+    renewableLikes = -1;
+  } else {
+    await Promise.all(
+      myRecommandations.data.map(async (recommandation) => {
         try {
-            if (renewableLikes === -1 || renewableLikes > 0) {
-                const remainingLike = await handleRecommandation(recommandation.content.user, savePictures, criterias);
-                renewableLikes = remainingLike;
-            } else {
-                console.log(colors.bgRed, "no more likes available", renewableLikes, colors.reset);
-                renewableLikes = 0;
-            }
-        } catch (error) {
-            console.log(colors.bgRed, `handleRecommandation failed ${error.message}`, colors.reset);
+          if (renewableLikes === -1 || renewableLikes > 0) {
+            const remainingLike = await handleRecommandation(
+              recommandation.content.user,
+              savePictures,
+              criterias
+            );
+            renewableLikes = remainingLike;
+          } else {
+            console.log(
+              colors.bgRed,
+              'no more likes available',
+              renewableLikes,
+              colors.reset
+            );
             renewableLikes = 0;
+          }
+        } catch (error) {
+          console.log(
+            colors.bgRed,
+            `handleRecommandation failed ${error.message}`,
+            colors.reset
+          );
+          renewableLikes = 0;
         }
-    }));
-    return renewableLikes;
+      })
+    );
+  }
+  return renewableLikes;
 }
 
 module.exports = processRecommandations;
--- src/core/start.js
+++ src/core/start.js
@@ -2,21 +2,28 @@ const colors = require('../utils/colors');
 const login = require('./login');
 const processRecommandations = require('./processRecommandations');
 
-async function start(savePictures, criterias) {
-    const loginSuccess = await login();
-    if (loginSuccess) {
-        await recommandationLoop(savePictures, criterias);
-    } else {
-        console.log(colors.bgRed, "login failed", colors.reset);
-    }
-}
 async function recommandationLoop(savePictures, criterias) {
-    const remainingLikes = await processRecommandations(savePictures, criterias);
-    if (remainingLikes > 0) {
-        await recommandationLoop(savePictures, criterias)
-    } else {
-        console.log(colors.bgRed, "no more likes for the moment", colors.reset);
-    }
+  const remainingLikes = await processRecommandations(savePictures, criterias);
+  if (remainingLikes === -1) {
+    console.log(
+      colors.bgRed,
+      'no recommandations available for the moment',
+      colors.reset
+    );
+  } else if (remainingLikes > 0) {
+    await recommandationLoop(savePictures, criterias);
+  } else {
+    console.log(colors.bgRed, 'no more likes for the moment', colors.reset);
+  }
+}
+
+async function start(savePictures, criterias) {
+  const loginSuccess = await login();
+  if (loginSuccess) {
+    await recommandationLoop(savePictures, criterias);
+  } else {
+    console.log(colors.bgRed, 'login failed', colors.reset);
+  }
 }
 
 module.exports = start;
