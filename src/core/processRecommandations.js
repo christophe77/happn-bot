98bd02c377d2020cb10e00c6ba0f46a074d0e545
christophe bellec <christophe.bellec@ekim.fr>
2022-06-06 11:50:02 +0200

    added timeout

--- src/core/processRecommandations.js
+++ src/core/processRecommandations.js
@@ -1,5 +1,6 @@
 const { recommandations } = require('happn-api').default;
 const colors = require('../utils/colors');
+const timeout = require('../utils/timeout');
 const handleRecommandation = require('./handleRecommandation');
 
 async function processRecommandations(savePictures, criterias) {
@@ -8,35 +9,36 @@ async function processRecommandations(savePictures, criterias) {
   if (myRecommandations.data?.length === 0) {
     renewableLikes = -1;
   } else {
-    await Promise.all(
-      myRecommandations.data.map(async (recommandation) => {
-        try {
-          if (renewableLikes === -1 || renewableLikes > 0) {
-            const remainingLike = await handleRecommandation(
-              recommandation.content.user,
-              savePictures,
-              criterias
-            );
-            renewableLikes = remainingLike;
-          } else {
-            console.log(
-              colors.bgRed,
-              'no more likes available',
-              renewableLikes,
-              colors.reset
-            );
-            renewableLikes = 0;
-          }
-        } catch (error) {
+    for (const recommandation of myRecommandations.data) {
+      try {
+        await timeout(2000);
+        if (renewableLikes === -1 || renewableLikes > 0) {
+          const remainingLike = await handleRecommandation(
+            recommandation.content.user,
+            savePictures,
+            criterias
+          );
+          renewableLikes = remainingLike;
+        } else {
           console.log(
             colors.bgRed,
-            `handleRecommandation failed ${error.message}`,
+            'no more likes available',
+            renewableLikes,
             colors.reset
           );
           renewableLikes = 0;
+          break;
         }
-      })
-    );
+      } catch (error) {
+        console.log(
+          colors.bgRed,
+          `handleRecommandation failed ${error.message}`,
+          colors.reset
+        );
+        renewableLikes = 0;
+        break;
+      }
+    }
   }
   return renewableLikes;
 }
--- src/core/processShortList.js
+++ src/core/processShortList.js
@@ -1,5 +1,6 @@
 const getShortList = require('./shortList');
 const colors = require('../utils/colors');
+const timeout = require('../utils/timeout');
 const handleShortList = require('./handleShortList');
 
 async function processShortList(savePictures, criterias) {
@@ -8,35 +9,36 @@ async function processShortList(savePictures, criterias) {
   if (myShortList.data?.shortlist_users?.length === 0) {
     renewableLikes = -1;
   } else {
-    await Promise.all(
-      myShortList.data.shortlist_users.map(async (recommandation) => {
-        try {
-          if (renewableLikes === -1 || renewableLikes > 0) {
-            const remainingLike = await handleShortList(
-              recommandation.content.user,
-              savePictures,
-              criterias
-            );
-            renewableLikes = remainingLike;
-          } else {
-            console.log(
-              colors.bgRed,
-              'no more likes available',
-              renewableLikes,
-              colors.reset
-            );
-            renewableLikes = 0;
-          }
-        } catch (error) {
+    for (const recommandation of myShortList.data.shortlist_users) {
+      try {
+        await timeout(2000);
+        if (renewableLikes === -1 || renewableLikes > 0) {
+          const remainingLike = await handleShortList(
+            recommandation.user,
+            savePictures,
+            criterias
+          );
+          renewableLikes = remainingLike;
+        } else {
           console.log(
             colors.bgRed,
-            `handleShortList failed ${error.message}`,
+            'no more likes available',
+            renewableLikes,
             colors.reset
           );
           renewableLikes = 0;
+          break;
         }
-      })
-    );
+      } catch (error) {
+        console.log(
+          colors.bgRed,
+          `handleShortList failed ${error.message}`,
+          colors.reset
+        );
+        renewableLikes = 0;
+        break;
+      }
+    }
   }
   return renewableLikes;
 }
