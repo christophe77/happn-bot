7c869c634858628f68b23be37c2a4cdcc6b5a158
christophe bellec <christophe.bellec@ekim.fr>
2022-06-23 08:36:26 +0200

    1

--- src/core/login.js
+++ src/core/login.js
@@ -2,13 +2,13 @@ const dotenv = require('dotenv');
 const { auth } = require('happn-api').default;
 
 async function login() {
-    dotenv.config();
-    const { FACEBOOK_EMAIL, FACEBOOK_PASSWORD } = process.env;
-    const facebookCredentials = {
-        email: FACEBOOK_EMAIL,
-        password: FACEBOOK_PASSWORD,
-    };
-    return auth.withFacebook(facebookCredentials);
+  dotenv.config();
+  const { FACEBOOK_EMAIL, FACEBOOK_PASSWORD } = process.env;
+  const facebookCredentials = {
+    email: FACEBOOK_EMAIL,
+    password: FACEBOOK_PASSWORD,
+  };
+  return auth.withFacebook(facebookCredentials);
 }
 
 module.exports = login;
